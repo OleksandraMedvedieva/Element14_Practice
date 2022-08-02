59fc88fcf3e0ca37ad632dd9a2aecccc6b028b19
Nataliia Dmytrenko <ednata82@gmail.com>
2022-08-02 22:06:51 +0300

    new f Nat

---
+++ src/newfeatureNataliia/HelloNat.java
@@ -0,0 +1,11 @@
+package newfeatureNataliia;
+
+public class HelloNat {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+        System.out.println("Hello2");
+    }
+}
